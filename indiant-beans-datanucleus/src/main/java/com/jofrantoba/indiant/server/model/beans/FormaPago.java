0eff57c767d45e8d989fa62203cee69d478b4759
jtorres <jtorres@colegiosperuanos.edu.pe>
2021-05-28 09:59:31 -0500

    use datanuclues

---
+++ indiant-beans-datanucleus/src/main/java/com/jofrantoba/indiant/server/model/beans/FormaPago.java
@@ -0,0 +1,120 @@
+package com.jofrantoba.indiant.server.model.beans;
+
+
+import com.jofrantoba.indiant.server.shared.beans.InterEntity;
+import javax.jdo.annotations.NotPersistent;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(detachable = "true")
+public class FormaPago implements InterEntity{
+	
+	/**
+	 * 
+	 */
+	
+	@PrimaryKey
+	@Persistent
+		
+	private String idFormaPago;
+	@Persistent
+	private String codeFormaPago;
+	@Persistent
+	private String tipo;
+	@Persistent
+	private Long version;
+	@NotPersistent
+	private String operacion;
+	@Persistent
+	private Boolean isPersistente;
+	
+	public String getIdFormaPago() {
+		return idFormaPago;
+	}
+	public void setIdFormaPago(String idFormaPago) {
+		this.idFormaPago = idFormaPago;
+	}
+	public String getCodeFormaPago() {
+		return codeFormaPago;
+	}
+	public void setCodeFormaPago(String codeFormaPago) {
+		this.codeFormaPago = codeFormaPago;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public Long getVersion() {
+		return version;
+	}
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+	public String getOperacion() {
+		return operacion;
+	}
+	public void setOperacion(String operacion) {
+		this.operacion = operacion;
+	}
+	public Boolean getIsPersistente() {
+		return isPersistente;
+	}
+	public void setIsPersistente(Boolean isPersistente) {
+		this.isPersistente = isPersistente;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((codeFormaPago == null) ? 0 : codeFormaPago.hashCode());
+		result = prime * result
+				+ ((idFormaPago == null) ? 0 : idFormaPago.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FormaPago other = (FormaPago) obj;
+		if (codeFormaPago == null) {
+			if (other.codeFormaPago != null)
+				return false;
+		} else if (!codeFormaPago.equals(other.codeFormaPago))
+			return false;
+		if (idFormaPago == null) {
+			if (other.idFormaPago != null)
+				return false;
+		} else if (!idFormaPago.equals(other.idFormaPago))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("FormaPago [idFormaPago=");
+		builder.append(idFormaPago);
+		builder.append(", codeFormaPago=");
+		builder.append(codeFormaPago);
+		builder.append(", tipo=");
+		builder.append(tipo);
+		builder.append(", version=");
+		builder.append(version);
+		builder.append(", operacion=");
+		builder.append(operacion);
+		builder.append(", isPersistente=");
+		builder.append(isPersistente);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+
+}
---
+++ indiant-shared/src/main/java/com/jofrantoba/indiant/server/shared/beans/InterEntity.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jofrantoba.indiant.server.shared.beans;
+
+import java.io.Serializable;
+
+
+
+/**
+ *
+ * @author jona
+ */
+public interface InterEntity extends Serializable {
+    
+}
